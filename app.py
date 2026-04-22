80521cca0a300563abef4e504e783fc092a1863f
Sebastian Klemkosky <sebastianklemkosky@gmail.com>
2026-04-22 11:13:09 -0500

    modified app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h1>Hello World</h1><p>Smart Reminder is running2.</p>"
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
