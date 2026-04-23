832bbf9d9cb552182d9917833b3f09a61d33affa
Sebastian Klemkosky <sebastianklemkosky@gmail.com>
2026-04-23 03:46:18 -0500

    Starting working on the general flow of the project
    need to finish the about page and add the rest of the pages.
     Also need to add the css file and link it to the html files.

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/add")
+def add_reminder():
+    return render_template("add_reminder.html")
+
+@app.route("/reminders")
+def view_reminders():
+    return render_template("view_reminders.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
